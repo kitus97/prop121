a61a4db53fef01b308960d00d9a04b952228f167
victor.chimaobi.ramirez <victor.chimaobi.ramirez@estudiantat.upc.edu>
2024-10-16 15:31:13 +0000

    Update file estanteria.java

--- src/main/java/com/estanteria.java
+++ src/main/java/com/estanteria.java
@@ -1,6 +1,29 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Estanteria {
+
+    private String name;
+    private ArrayList<Set<String>> distribution;
 
 
-public class Supermercado{
-    public string name;
-    
+    public Estanteria(String n, int t) {
+        name = n;
+        distribution = new ArrayList<Set<String>>();
+        for (int i = 0; i < t; ++i) {
+            distribution.add(new HashSet<String>());
+        }
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Set<String>> getDistribution() {
+        return distribution;
+    }
+        
+
 }
