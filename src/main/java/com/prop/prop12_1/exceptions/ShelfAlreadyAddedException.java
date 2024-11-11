fa56fef3bef1e0a5eca38ea311d048b7fcefcf67
Edexel <victor.chimaobi.ramirez@estudiantat.upc.edu>
2024-11-11 20:52:57 +0100

    Añadidas dos exceptions.

--- src/main/java/com/prop/prop12_1/exceptions/ShelfAlreadyAddedException.java
+++ src/main/java/com/prop/prop12_1/exceptions/ShelfAlreadyAddedException.java
@@ -1,6 +1,6 @@
 package com.prop.prop12_1.exceptions;
 
-public class ShelfAlreadyAddedException extends RuntimeException {
+public class ShelfAlreadyAddedException extends RuntimeException  {
     public ShelfAlreadyAddedException(String message) {
         super(message);
     }
