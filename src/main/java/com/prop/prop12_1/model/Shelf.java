0bcef8abf40d16cce32682a5b06042f455cd2ba0
Edexel <victor.chimaobi.ramirez@estudiantat.upc.edu>
2024-11-14 12:42:38 +0100

    Modificada shelf para que haga uso de excepciones.

--- src/main/java/com/prop/prop12_1/model/Shelf.java
+++ src/main/java/com/prop/prop12_1/model/Shelf.java
@@ -27,31 +27,35 @@ public class Shelf{
         return distribution;
     }
 
-    /*Pre: s es el nombre de una caracteristica valida y t <= distribution.size()
+    /*Pre: restriction es el nombre de una caracteristica valida y t <= distribution.size()
      */
-    public void setRestriction(String s, int t){
-        distribution.get(t).add(s);
+    public void setRestriction(String restriction, int t){
+        if(distribution.size() <= t) throw new IndexOutOfBoundsException("The index is out of range");
+        else distribution.get(t).add(restriction);
     }
 
     public void deleteRestrictions(int t){
-        distribution.get(t).clear();
+        if(distribution.size() <= t) throw new IndexOutOfBoundsException("The index is out of range");
+        else distribution.get(t).clear();
     }
 
-    /*Pre: t > 0;
-     */
-    public void resizeShelf(int t){
-        if(distribution.size() < t){
-            for(int i = distribution.size(); i < t; ++i){
-                distribution.add(new HashSet<String>());
-            }
-        }
-        else{
-            for(int i = distribution.size() - 1; i >= t; --i){
-                distribution.remove(i);
+
+    public void resizeShelf(int t) {
+        if (t < 0) throw new IndexOutOfBoundsException("Invalid size");
+
+        else {
+            if (distribution.size() < t) {
+                for (int i = distribution.size(); i < t; ++i) {
+                    distribution.add(new HashSet<String>());
+                }
+            } else {
+                for (int i = distribution.size() - 1; i >= t; --i) {
+                    distribution.remove(i);
+                }
+
             }
 
         }
-
     }
 
 
